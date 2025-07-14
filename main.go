08581504e874f673dcab8b326aab3bf1652410b3
Keir Whitlock <keir.whitlock@groundcontrol.com>
2025-07-14 23:15:58 +0100

    End of Section 3

--- main.go
+++ main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-
-	// similar to fmt.Println() but you can sat where to print
-	// in this case to the ResponseWriter w.
 	fmt.Fprint(w, "<h1>Welcome to my website!</h1>")
 }
 
@@ -29,51 +28,35 @@ func teapotHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Teapot</h1></br>")
 }
 
-//func pathHandler(w http.ResponseWriter, r *http.Request) {
-//	switch r.URL.Path {
-//	case "/teapot":
-//		teapotHandler(w, r)
-//	case "/contact":
-//		contactHandler(w, r)
-//	case "/faq":
-//		faqHandler(w, r)
-//	case "/":
-//		homeHandler(w, r)
-//	default:
-//		w.WriteHeader(http.StatusNotFound)
-//		fmt.Fprint(w, "<h1>404 Not Found</h1>")
-//	}
-//}
+func getGallery(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
 
-type Router struct{}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Welcome to the Gallery!</h1>")
+	fmt.Fprintf(w, "<p>The id was: %s</p>", id)
 
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/teapot":
-		teapotHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/faq":
-		faqHandler(w, r)
-	case "/":
-		homeHandler(w, r)
-	default:
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>404 Not Found</h1>")
-	}
 }
 
 func main() {
-	//var router http.HandlerFunc
-	//router = pathHandler
+	r := chi.NewRouter()
 
-	//http.HandleFunc("/", homeHandler) // uses the default servemux
-	//http.HandleFunc("/contact", contactHandler)
-	//
-	var router Router
+	// chi middleware
+	// https://go-chi.io/#/pages/middleware?id=logger
+	r.Use(middleware.Logger)
+
+	// chi routes
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.Get("/teapot", teapotHandler)
+
+	// https://go-chi.io/#/pages/routing?id=routing-patterns-amp-url-parameters
+	r.Get("/galleries/{id}", getGallery)
+
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "404 page not found", http.StatusNotFound)
+	})
 
 	fmt.Println("Starting the server on port :3000")
-	//http.ListenAndServe(":3000", nil) // passing in nil as a Handler, uses the DefaultServeMux
-	//http.ListenAndServe(":3000", http.HandlerFunc(pathHandler)) // passing in nil as a Handler, uses the DefaultServeMux
-	http.ListenAndServe(":3000", router) // passing in nil as a Handler, uses the DefaultServeMux
+	http.ListenAndServe(":3000", r) // passing in nil as a Handler, uses the DefaultServeMux
 }
